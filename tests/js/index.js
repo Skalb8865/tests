08b238630c605e27bfbb554fd836221c1275e9e8
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2024-11-14 14:38:34 -0500

    .

--- tests/js/index.js
+++ tests/js/index.js
@@ -3,33 +3,36 @@ const navMenu = document.getElementById("main-nav");
 const navOpen = document.getElementById("nav-open");
 const navClose = document.getElementById("nav-close");
 
+let changeButton = function (icon) {
+    icon.classList.toggle('fa-regular fa-circle-pause');
+}
 /*===== SHOW MENU =====*/
 navOpen.addEventListener("click", () => {
-	navMenu.classList.add("show-menu");
+    navMenu.classList.add("show-menu");
 });
 
 /*===== HIDE MENU =====*/
 navClose.addEventListener("click", () => {
-	navMenu.classList.remove("show-menu");
+    navMenu.classList.remove("show-menu");
 });
 
 // REMOVE MENU ON CLICK FOR MOBILE =========================
 const navLink = document.querySelectorAll(".nav-link");
 
 function linkAction() {
-	const navMenu = document.getElementById("main-nav");
+    const navMenu = document.getElementById("main-nav");
 
-	navMenu.classList.remove("show-menu");
+    navMenu.classList.remove("show-menu");
 }
 navLink.forEach((link) => link.addEventListener("click", linkAction));
 
 
 // CHANGE THE BACKGROUND ON SCROLL =========================
 function changeHeaderColor() {
-	const nav = document.getElementById("header");
+    const nav = document.getElementById("header");
 
-	if (this.scrollY >= 200) nav.classList.add("scroll-header");
-	else nav.classList.remove("scroll-header");
+    if (this.scrollY >= 200) nav.classList.add("scroll-header");
+    else nav.classList.remove("scroll-header");
 }
 window.addEventListener("scroll", changeHeaderColor);
 
@@ -40,40 +43,40 @@ const navLogo = document.querySelector('#navbar_logo');
 
 // Display Mobile Menu
 const mobileMenu = () => {
-  menu.classList.toggle('is-active');
-  menuLinks.classList.toggle('active');
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
 };
 
 menu.addEventListener('click', mobileMenu);
 
 // Show active menu when scrolling
 const highlightMenu = () => {
-  const elem = document.querySelector('.highlight');
-  const homeMenu = document.querySelector('#home-page');
-  const aboutMenu = document.querySelector('#about-page');
-  const servicesMenu = document.querySelector('#services-page');
-  let scrollPos = window.scrollY;
-  console.log(scrollPos);
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const aboutMenu = document.querySelector('#about-page');
+    const servicesMenu = document.querySelector('#services-page');
+    let scrollPos = window.scrollY;
+    console.log(scrollPos);
 
-  // adds 'highlight' class to my menu items
-  if (window.innerWidth > 960 && scrollPos < 600) {
-    homeMenu.classList.add('highlight');
-    aboutMenu.classList.remove('highlight');
-    return;
-  } else if (window.innerWidth > 960 && scrollPos < 1400) {
-    aboutMenu.classList.add('highlight');
-    homeMenu.classList.remove('highlight');
-    servicesMenu.classList.remove('highlight');
-    return;
-  } else if (window.innerWidth > 960 && scrollPos < 2345) {
-    servicesMenu.classList.add('highlight');
-    aboutMenu.classList.remove('highlight');
-    return;
-  }
+    // adds 'highlight' class to my menu items
+    if (window.innerWidth > 960 && scrollPos < 600) {
+        homeMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
+        return;
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+        aboutMenu.classList.add('highlight');
+        homeMenu.classList.remove('highlight');
+        servicesMenu.classList.remove('highlight');
+        return;
+    } else if (window.innerWidth > 960 && scrollPos < 2345) {
+        servicesMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
+        return;
+    }
 
-  if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
-    elem.classList.remove('highlight'); 
-  }
+    if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+        elem.classList.remove('highlight');
+    }
 };
 
 window.addEventListener('scroll', highlightMenu);
@@ -81,18 +84,13 @@ window.addEventListener('click', highlightMenu);
 
 //  Close mobile Menu when clicking on a menu item
 const hideMobileMenu = () => {
-  const menuBars = document.querySelector('.is-active');
-  if (window.innerWidth <= 768 && menuBars) {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.remove('active');
-  }
+    const menuBars = document.querySelector('.is-active');
+    if (window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }
 };
 
 menuLinks.addEventListener('click', hideMobileMenu);
 navLogo.addEventListener('click', hideMobileMenu);
 /*--------------------------------------END OF album1,2,3 js--------------------------------*/
-/*--ON CLICK------------------------*/
-let changeButton = function(icon){
-  icon.classList.toggle('fa-circle-pause')
-}
-/*--ON CLICK------------------------*/
