153194007d4689140e8aff3fdd0292a1892882b7
EquinoxEgo <goodenaidien@Gmail.com>
2024-12-10 14:06:20 -0500

    shopping
    I'm tired and annoyed I cant find a shopping cart I'm giving this task to Sean

--- tests/js/cart.js
+++ tests/js/cart.js
@@ -1,157 +1,111 @@
 
 //------Start OF SHOPPING CART-----------------------------------------------------//  
-/* jslint browser: true*/
-/*global $*/
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
 
-// https://github.com/jasonmoo/t.js
-(function(){function c(a){this.t=a}function l(a,b){for(var e=b.split(".");e.length;){if(!(e[0]in a))return!1;a=a[e.shift()]}return a}function d(a,b){return a.replace(h,function(e,a,i,f,c,h,k,m){var f=l(b,f),j="",g;if(!f)return"!"==i?d(c,b):k?d(m,b):"";if(!i)return d(h,b);if("@"==i){e=b._key;a=b._val;for(g in f)f.hasOwnProperty(g)&&(b._key=g,b._val=f[g],j+=d(c,b));b._key=e;b._val=a;return j}}).replace(k,function(a,c,d){return(a=l(b,d))||0===a?"%"==c?(new Option(a)).innerHTML.replace(/"/g,"&quot;"):
-    a:""})}var h=/\{\{(([@!]?)(.+?))\}\}(([\s\S]+?)(\{\{:\1\}\}([\s\S]+?))?)\{\{\/\1\}\}/g,k=/\{\{([=%])(.+?)\}\}/g;c.prototype.render=function(a){return d(this.t,a)};window.t=c})();
-    // end of 't';
-    
-    Number.prototype.to_$ = function () {
-      return "$" + parseFloat( this ).toFixed(2);
-    };
-    String.prototype.strip$ = function () {
-      return this.split("$")[1];
-    };
-    
-    var app = {
-    
-      shipping : 5.00,
-      products : [
-          {
-            "name" : "Meet and greet with Bill Murray",
-            "price" : "19.95",
-            "img" : "http://fillmurray.com/g/159/159",
-            "desc" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-          },
-          {
-            "name" : "Tap Water",
-            "price" : "0.99",
-            "img" : "http://shechive.files.wordpress.com/2011/08/odd-products-28.jpg?w=159&h=159",
-            "desc" : "You decide"
-          },
-          {
-            "name" : "Social Shower Curtain",
-            "price" : "40.00",
-            "img" : "http://media-cache-ec2.pinimg.com/550x/c7/02/8f/c7028f260ae030ba66494ee75407192f.jpg",
-            "desc" : "Always stay up-to-date with this very useless shower curtain!"
-          },
-          {
-            "name" : "Self stirring mug",
-            "img" : "https://i.dailymail.co.uk/i/pix/2012/09/16/article-2204256-15053801000005DC-304_634x524.jpg",
-            "price" : "12.35",
-            "desc" : "Don't get tired!"
-          }
-        ],
-    
-      removeProduct: function () {
-        "use strict";
-    
-        var item = $(this).closest(".shopping-cart--list-item");
-    
-        item.addClass("closing");
-        window.setTimeout( function () {
-          item.remove();
-          app.updateTotals();
-        }, 500); // Timeout for css animation
-      },
-    
-      addProduct: function () {
-        "use strict";
-    
-        var qtyCtr = $(this).prev(".product-qty"),
-            quantity = parseInt(qtyCtr.html(), 10) + 1;
-    
-        app.updateProductSubtotal(this, quantity);
-      },
-    
-      subtractProduct: function () {
-        "use strict";
-    
-        var qtyCtr = $(this).next(".product-qty"),
-            num = parseInt(qtyCtr.html(), 10) - 1,
-            quantity = num <= 0 ? 0 : num;
-    
-        app.updateProductSubtotal(this, quantity);
-      },
-    
-      updateProductSubtotal: function (context, quantity) {
-        "use strict";
-    
-        var ctr = $(context).closest(".product-modifiers"),
-            productQtyCtr = ctr.find(".product-qty"),
-            productPrice = parseFloat(ctr.data("product-price")),
-            subtotalCtr = ctr.find(".product-total-price"),
-            subtotalPrice = quantity * productPrice;
-    
-        productQtyCtr.html(quantity);
-        subtotalCtr.html( subtotalPrice.to_$() );
-    
-        app.updateTotals();
-      },
-    
-      updateTotals: function () {
-        "use strict";
-    
-        var products = $(".shopping-cart--list-item"),
-            subtotal = 0,
-            shipping;
-    
-        for (var i = 0; i < products.length; i += 1) {
-          subtotal += parseFloat( $(products[i]).find(".product-total-price").html().strip$() );
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'PRODUCT NAME 1',
+        image: '1.PNG',
+        price: 120000
+    },
+    {
+        id: 2,
+        name: 'PRODUCT NAME 2',
+        image: '2.PNG',
+        price: 120000
+    },
+    {
+        id: 3,
+        name: 'PRODUCT NAME 3',
+        image: '3.PNG',
+        price: 220000
+    },
+    {
+        id: 4,
+        name: 'PRODUCT NAME 4',
+        image: '4.PNG',
+        price: 123000
+    },
+    {
+        id: 5,
+        name: 'PRODUCT NAME 5',
+        image: '5.PNG',
+        price: 320000
+    },
+    {
+        id: 6,
+        name: 'PRODUCT NAME 6',
+        image: '6.PNG',
+        price: 120000
+    }
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Card</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        // copy product form list to list card
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
         }
-    
-        shipping = (subtotal > 0 && subtotal < (100 / 1.06)) ? app.shipping : 0;
-    
-        $("#subtotalCtr").find(".cart-totals-value").html( subtotal.to_$() );
-        $("#taxesCtr").find(".cart-totals-value").html( (subtotal * 0.06).to_$() );
-        $("#totalCtr").find(".cart-totals-value").html( (subtotal * 1.06 + shipping).to_$() );
-        $("#shippingCtr").find(".cart-totals-value").html( shipping.to_$() );
-      },
-    
-      attachEvents: function () {
-        "use strict";
-    
-        $(".product-remove").on("click", app.removeProduct);
-        $(".product-plus").on("click", app.addProduct);
-        $(".product-subtract").on("click", app.subtractProduct);
-      },
-    
-      setProductImages: function () {
-        "use strict";
-    
-        var images = $(".product-image"),
-            ctr,
-            img;
-    
-        for (var i = 0; i < images.length; i += 1) {
-          ctr = $(images[i]),
-          img = ctr.find(".product-image--img");
-    
-          ctr.css("background-image", "url(" + img.attr("src") + ")");
-          img.remove();
-        }
-      },
-    
-      renderTemplates: function () {
-        "use strict";
-    
-        var products = app.products,
-            content = [],
-            template = new t( $("#shopping-cart--list-item-template").html() );
-    
-        for (var i = 0; i < products.length; i += 1) {
-          content[i] = template.render(products[i]);
-        }
-    
-        $("#shopping-cart--list").html(content.join(""));
-      }
-    
-    };
-    
-    app.renderTemplates();
-    app.setProductImages();
-    app.attachEvents();
-    
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
 //------END OF SHOPPING CART-----------------------------------------------------//  
